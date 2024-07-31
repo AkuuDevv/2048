4793207f5d6e744e26a466cd5dae4a97fee9a88e
luckyloo1730 <luckyloo1730@gmail.com>
2024-07-31 20:07:36 +0300

    added script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// const { Button } = require("@headlessui/react");
+// const Game = require("./Game");
+
 class Game {
   static STATUS = {
     idle: 'idle',
@@ -361,4 +364,60 @@ class Game {
     }
   }
 }
-module.exports = Game
+// module.exports = Game
+
+const game = new Game()
+const startButton = document.querySelector("button")
+const score = document.querySelector(".score")
+
+function getScore() {
+  score.innerHTML = game.getScore()
+}
+
+startButton.addEventListener("click", () => {
+  if (startButton.classList.contains('start')) {
+    game.start()
+    startButton.classList = "button restart"
+    startButton.innerHTML = `<svg height="24"
+      width="24"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
+        fill="currentColor"></path></svg>
+    <span>ReLaunch</span>`
+    getScore()
+  }else if (startButton.classList.contains = "restart") {
+    game.restart()
+    startButton.classList = "button start"
+    startButton.innerHTML = `<svg height="24"
+      width="24"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
+        fill="currentColor"></path></svg>
+    <span>Launch</span>`
+    getScore()
+  }
+})
+
+document.addEventListener('keydown', (e) => {
+  switch (e.key) {
+      case 'ArrowLeft':
+          game.moveLeft();
+          break;
+      case 'ArrowRight':
+          game.moveRight();
+          break;
+      case 'ArrowUp':
+          game.moveUp();
+          break;
+      case 'ArrowDown':
+          game.moveDown();
+          break;
+  }
+
+  getScore();
+});
+
